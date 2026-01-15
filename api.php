a0e6310f1d4f34f32c2ce323787d51ad87673f1a
EasyPlatformDEV <bg.easyplatform@gmail.com>
2026-01-15 13:21:04 +0200

    Add files via upload

--- api.php
+++ api.php
@@ -10,7 +10,7 @@ $products = [
         "title" => "Samsung GALAXY телефон A5 256GB Navy",
         "price" => 1299.85,
         "currency" => "$",
-        "image" => "https://via.placeholder.com/80x120",
+        "image" => "https://cdn.mydreamshop.io/resized/private/pcshop/products/images/9e110b8cb36c5105c8e1d5cfe3b7525a6c500222/C/173755485148026790fba3753f0509.png",
         "shop" => ["name" => "samsung-shop.bg", "icon" => "https://via.placeholder.com/24"],
         "hasAlert" => false
     ],
@@ -20,7 +20,7 @@ $products = [
         "title" => "Samsung Galaxy S23 Ultra - Graphite",
         "price" => 1850.00,
         "currency" => "лв.",
-        "image" => "https://via.placeholder.com/80x120",
+        "image" => "https://cdn.mydreamshop.io/resized/private/pcshop/products/images/9e110b8cb36c5105c8e1d5cfe3b7525a6c500222/C/173755485148026790fba3753f0509.png",
         "shop" => ["name" => "ozone.bg", "icon" => "https://via.placeholder.com/24"],
         "hasAlert" => true,
         "alertData" => [
