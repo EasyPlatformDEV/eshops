b8bb573de669584f27fbf9387d8d41cfa1b4d3cf
EasyPlatformDEV <bg.easyplatform@gmail.com>
2026-01-25 12:36:06 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -764,7 +764,7 @@
             if ((isSearchPage && index === 0) || !isSearchPage) {
                 statusIcon = `
                  <svg viewBox="0 0 24 24" width="24" height="24" class="icon-favorite product-card-favorite">
-                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" fill="currentColor" />
+                    <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" fill="#b60378" />
                  </svg>`;
             }
 
